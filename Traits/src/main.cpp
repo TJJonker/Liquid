4f21626efebfee2b735b5a2580031ce8a6757832
Tom Jonker <tjj.jonker@gmail.com>
2025-02-06 14:25:33 +0100

    Traits Hello World

--- Traits/src/main.cpp
+++ Traits/src/main.cpp
@@ -1,5 +1,9 @@
 #include "pch.h"
 
+#include <type_traits>
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	std::cout << std::boolalpha; // Changes 0 to false and others to true in output.
+	std::cout << "Is int integral? " << std::is_integral<int>::value << std::endl; // Checks if int is an integral
+	std::cout << "Is int integral? " << std::is_integral<float>::value << std::endl; // Checks if float is an integral
 }
