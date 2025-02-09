1ea72c5abc568f2fa55f33ab7734fa2e3c953317
Tom Jonker <tjj.jonker@gmail.com>
2025-02-09 17:14:20 +0100

    Added enable_if functionality

--- Traits/src/main.cpp
+++ Traits/src/main.cpp
@@ -1,24 +1,11 @@
 #include "pch.h"
-
 #include <type_traits>
 
-template <typename T>
-struct has_toString {
-	template <typename U>
-	static auto test(int) -> decltype(std::declval<U>().toString(), std::true_type{});
-
-	template <typename>
-	static std::false_type test(...);
-
-public:
-	static constexpr bool value = decltype(test<T>(0))::value;
-};
+template<typename T>
+struct is_pointer : std::is_pointer<T> {};
 
 int main() {
-	struct A { std::string toString() const { return "A"; } };
-	struct B {};
-
-	std::cout << has_toString<A>::value << std::endl;  // Should print 1 (true)
-	std::cout << has_toString<B>::value << std::endl;  // Should print 0 (false)
-
+	std::cout << std::boolalpha;
+	std::cout << is_pointer<int*>::value << std::endl;  // Should print 1 (true)
+	std::cout << is_pointer<int>::value << std::endl;   // Should print 0 (false)
 }
