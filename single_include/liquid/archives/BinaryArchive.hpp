f199ec0efe0ec8c65999486f88002d35c79755bd
Tom Jonker <tjj.jonker@gmail.com>
2025-02-25 12:35:12 +0100

    Start minor rework

--- single_include/liquid/archives/BinaryArchive.hpp
+++ single_include/liquid/archives/BinaryArchive.hpp
@@ -10,9 +10,8 @@ namespace Liquid {
 
 		~BinaryOutputArchive() { }
 
-		template<typename T>
-		void ProcessImpl(T&& data) {
-			_stream.write((const char*)&data, sizeof(T));
+		void WriteData(const void* data, size_t size) {
+			_stream.write((const char*)& data, size);
 		}
 
 	private:
@@ -26,12 +25,78 @@ namespace Liquid {
 
 		~BinaryInputArchive() { }
 
-		template<typename T>
-		void ProcessImpl(T&& data) {
-			_stream.read((char*)&data, sizeof(T));
+		void ReadData(void const* data, size_t size) {
+			_stream.read((char*)&data, size);
 		}
 
 	private:
+
 		std::istream& _stream;
 	};
+
+	// =======================================
+	// OutputArchive
+	// =======================================
+
+	template<typename T>
+	void Prologue(BinaryOutputArchive& a, ArrayRef<T>&& ar) {
+		std::cout << "Prologueing Array" << std::endl;
+		a.WriteData(ar.size, sizeof(size_t));
+	}
+
+	template<typename T>
+	void Epilogue(BinaryOutputArchive& a, ArrayRef<T>&& ar) {
+		std::cout << "Epilogueing Array" << std::endl;
+	}
+
+	// =======================================
+
+	template<class Archive, typename T,
+		typename std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
+	void Prologue(Archive&, T const& something) {
+		std::cout << "Prologueing arithmetic" << std::endl;
+	}
+
+	template<class Archive, typename T>
+	void Epilogue(Archive&, T const&) {
+		std::cout << "Epilogueing arithmetic" << std::endl;
+	}
+
+	// =======================================
+
+	template<typename T, typename std::enable_if_t<is_serializable_v<T>, int> = 0>
+	void Prologue(BinaryOutputArchive& a, T const&) {
+		std::cout << "Prologueing Serializable" << std::endl;
+	}
+
+	template<typename T, typename std::enable_if_t<is_serializable_v<T>, int> = 0>
+	void Epilogue(BinaryOutputArchive& a, T const&) {
+		std::cout << "Epilogueing Serializable" << std::endl;
+	}
+
+	// =======================================
+	// InputArchive
+	// =======================================
+
+	template<typename T>
+	void Prologue(BinaryInputArchive& a, ArrayRef<T>&& ar) {
+		std::cout << "Prologueing Array" << std::endl;
+	}
+
+	template<typename T>
+	void Epilogue(BinaryInputArchive& a, ArrayRef<T>&& ar) {
+		std::cout << "Epilogueing Array" << std::endl;
+	}
+
+	// =======================================
+
+	template<typename T, typename std::enable_if_t<is_serializable_v<T>, int> = 0>
+	void Prologue(BinaryInputArchive& a, T const&) {
+		std::cout << "Prologueing Serializable" << std::endl;
+	}
+
+	template<typename T, typename std::enable_if_t<is_serializable_v<T>, int> = 0>
+	void Epilogue(BinaryInputArchive& a, T const&) {
+		std::cout << "Epilogueing Serializable" << std::endl;
+	}
 }
