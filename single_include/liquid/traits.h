71096052fc0f0ae529a8d5cd7d54c6754efbaeb3
Tom Jonker <tjj.jonker@gmail.com>
2025-03-05 12:05:57 +0100

    Turned function check into a Macro
    This allows us to create method-check functions with a Macro, as seen on line 38

--- single_include/liquid/traits.h
+++ single_include/liquid/traits.h
@@ -3,8 +3,8 @@
 
 namespace Liquid::traits {
 
-    typedef std::false_type no;
-    typedef std::true_type yes;
+    using no = std::false_type;
+    using yes = std::true_type;
 
 #pragma region Serialization Check
 
@@ -21,35 +21,19 @@ namespace Liquid::traits {
 
 #pragma endregion
 
-    //#define LIQUID_MAKE_MEMBER_TEST(name_to_test)                                                                         \
-    //namespace detail {                                                                                                          \
-    //    template<class T, class A>                                                                                              \
-    //    struct has_member_##name_to_test##_impl {                                                                               \
-    //    template <class TT, class AA>                                                                                           \
-    //    static auto test(int) -> access::member_##name_to_test(std::declval<AA&>(), std::declval<TT&>()), yes());               \
-    //    template<class, class>                                                                                                  \
-    //    static no test(...);                                                                                                    \
-    //    static const bool value = test(0)::value;                                                                               \
-    //    };                                                                                                                      \
-    //}                                                                                                                           \
-    //template<class T, class A>                                                                                                  \
-    //struct has_member_##name_to_test : std::integral_constant<bool, detail::has_member_##name_to_test##_impl<T, A>::value> {}
-
-    //LIQUID_MAKE_MEMBER_TEST(save);
-
-    namespace detail {
-        template<class T, class A>                                                                                              
-        struct has_archive_save_impl {
-            template <class TT, class AA>                                                                                           
-            static auto test(int) -> decltype(Save(std::declval<AA&>(), std::declval<TT&>()), yes()); 
-
-            template<class, class>                                                                                                  
-            static no test(...);
-
-            static const bool value = std::is_same<decltype(test<T, A>(0)), yes>::value;
-		};
-	}
-
-	template<class T, class A>
+    #define LIQUID_MAKE_MEMBER_TEST(functionName)                                                                               \
+    namespace detail {                                                                                                          \
+        template<class T, class A>                                                                                              \
+        struct has_archive_##functionName##_impl {                                                                               \
+        template <class TT, class AA>                                                                                           \
+        static auto test(int) -> decltype(Save(std::declval<AA&>(), std::declval<TT&>()), yes());                               \
+        template<class, class>                                                                                                  \
+        static no test(...);                                                                                                    \
+        static const bool value = decltype(test<T, A>(0))::value;                                                               \
+        };                                                                                                                      \
+    }                                                                                                                           \
+    template<class T, class A>                                                                                                  \
     struct has_archive_save : std::integral_constant<bool, detail::has_archive_save_impl<T, A>::value> {};
+
+    LIQUID_MAKE_MEMBER_TEST(save);
 }
