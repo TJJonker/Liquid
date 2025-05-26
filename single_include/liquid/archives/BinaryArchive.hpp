3ddc45a4db774e8f2521b7f61d574bcba7b6cd8f
Tom Jonker <tjj.jonker@gmail.com>
2025-05-26 16:59:00 +0200

    Removed some unnecessary functionality

--- single_include/liquid/archives/BinaryArchive.hpp
+++ single_include/liquid/archives/BinaryArchive.hpp
@@ -6,37 +6,22 @@ namespace Liquid {
 	class BinaryOutputArchive : public OutputArchive<BinaryOutputArchive> {
 	public:
 		BinaryOutputArchive(std::ostream& stream) :
-			OutputArchive<BinaryOutputArchive>(this), _stream(stream) { }
+			_stream(stream) {}
 
-		~BinaryOutputArchive() { }
+		~BinaryOutputArchive() {}
 
 		void WriteData(const void* data, size_t size) {
-			_stream.write((const char*)& data, size);
+			_stream.write((const char*)&data, size);
 		}
 
 	private:
 		std::ostream& _stream;
 	};
 
-	//class BinaryInputArchive : public OutputArchive<BinaryInputArchive> {
-	//public:
-	//	BinaryInputArchive(std::istream& stream) :
-	//		OutputArchive<BinaryInputArchive>(this), _stream(stream) { }
-
-	//	~BinaryInputArchive() { }
-
-	//	void ReadData(void const* data, size_t size) {
-	//		_stream.read((char*)&data, size);
-	//	}
-
-	//private:
-	//	std::istream& _stream;
-	//};
-
 	template<typename T>
 	std::enable_if_t<std::is_arithmetic_v<T>>
-	save(BinaryOutputArchive& a, T& t) {
+		save(BinaryOutputArchive& a, T& t) {
+		a.WriteData((const void*)t, sizeof(T));
 		std::cout << "Saving a arithmetic value!" << std::endl;
 	}
 }
-
--- single_include/liquid/archives/archive.hpp
+++ single_include/liquid/archives/archive.hpp
@@ -9,8 +9,7 @@ namespace Liquid {
 	template<class Archive>
 	class OutputArchive {
 	public:
-		OutputArchive(Archive* const derived) : _self(derived) { }
-		virtual ~OutputArchive() = default;
+ 		virtual ~OutputArchive() = default;
 
 		template<class ... Types> inline
 			void operator()(Types && ... args) {
@@ -20,39 +19,19 @@ namespace Liquid {
 	private:
 		template<typename T>
 		void Process(T&& arg) {
-			Prologue(*_self, std::forward<T>(arg));
+			//Prologue(*_self, std::forward<T>(arg));
 			ProcessImpl(arg);
-			Epilogue(*_self, std::forward<T>(arg));
+			//Epilogue(*_self, std::forward<T>(arg));
 		}
 
 #define ENABLE_IF(name)														\
 		std::enable_if_t<traits::has_##name<T, Archive>::value, int> = 0
 
-		// Check if archive contains the function
-		// Check if archive if ambiguity free
-		//template<typename T, ENABLE_IF(serialize_member)>
-		//void ProcessImpl(T&& arg) {
-		//	std::cout << "Processing member serialization" << std::endl;
-		//}
-
-		template<typename T, ENABLE_IF(archive_serialize)>
-		void ProcessImpl(T&& arg) {
-			std::cout << "Processing archive serialization" << std::endl;
-			serialize(*_self, arg);
-		}
 
 		template<typename T, ENABLE_IF(archive_save)>
 		void ProcessImpl(T&& arg) {
-			save(*_self, arg);
+			save(*((Archive*)this), arg);
 		}
-
-		//template<typename T>
-		//void ProcessImpl(T&&) {
-		//	std::cout << "Error, no overload found" << std::endl;
-		//}
-
-	private:
-		Archive* const _self;
 	};
 
 	template<class Archive, typename T>
